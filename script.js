aa7fb91315639a2ff045357ce082c7139b331f5b
s10269187 <s10269187@connect.np.edu.sg>
2025-02-04 20:17:19 +0800

    homepg about to be done

--- script.js
+++ script.js
@@ -41,3 +41,45 @@ function showSlides(n) {
 setInterval(() => {
   plusSlides(1);
 }, 4000); // Change slide every 3 seconds
+
+document.addEventListener("DOMContentLoaded", function () {
+  // What kind of interface we want at the start 
+  const APIKEY = "67a0285d70c1ac5876db566f";
+
+  //[STEP 1]: Create our submit form listener
+  document.getElementById("submit").addEventListener("click", function (e) {
+    // Prevent default action of the button 
+    e.preventDefault();
+
+    //[STEP 2]: Let's retrieve form data
+    // For now, we assume all information is valid
+    // You are to do your own data validation
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+    //[STEP 3]: Get form values when the user clicks on send
+    // Adapted from restdb API
+    let jsondata = {
+      "email": email,
+      "password": password
+    };
+
+    //[STEP 4]: Create our AJAX settings. Take note of API key
+    let settings = {
+      method: "POST", //[cher] we will use post to send info
+      headers: {
+        "Content-Type": "application/json",
+        "x-apikey": APIKEY,
+        "Cache-Control": "no-cache"
+      },
+      body: JSON.stringify(jsondata),
+    }
+
+    //[STEP 5]: Send our AJAX request over to the DB and print response of the RESTDB storage to console.
+    fetch("https://aprilsonata-d713.restdb.io/rest/customer", settings)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      });
+  });//end click 
+});
